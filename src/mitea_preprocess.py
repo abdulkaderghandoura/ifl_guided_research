dcb8770174b83c40bd872614fe79bb807859d4b1
Abdulkader Ghandoura <abdulkader.ib.gh@gmail.com>
2024-08-20 13:12:41 +0200

    Added mesh generator

--- src/mitea_preprocess.py
+++ src/mitea_preprocess.py
@@ -2,17 +2,8 @@ from pathlib import Path
 from tqdm import tqdm
 import numpy as np
 import nibabel as nib
-import open3d as o3d
-import trimesh
 from skimage import measure
-from skimage.morphology import binary_dilation
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from scipy.spatial import Delaunay
-
-import pyvista as pv
-
-repo_dir_path = Path(__file__).resolve().parent.parent
+import cv2
 
 
 def load_nifti(file_path):
@@ -22,193 +13,102 @@ def load_nifti(file_path):
     return data, affine
 
 
-def extract_lv_contours(volume_data):
+def extract_contours(volume_data):
     """
     Extract LV contours from the MRI volume data where the mask equals 1.
     """
-    contours = []
+    inner_contours = []
+    outer_contours = []
     for i in range(volume_data.shape[0]):
         slice_data = volume_data[i, :, :]
         # Only consider the slice data where mask == 1
         binary_slice = (slice_data == 1)
         # Extract contours from this binary mask
-        contour = measure.find_contours(binary_slice, 0.5)
+        slice_contours = measure.find_contours(binary_slice, 0.5)
 
-        # Only append if contours are found
-        if contour:
-            contours.append(contour)
+        if len(slice_contours) == 1:
+            outer_contours.append(slice_contours[0])
+            inner_contours.append(np.array([]))
+        elif len(slice_contours) == 2:
+            slice_contours_0 = np.array(slice_contours[0], dtype=np.float32)
+            slice_contours_1 = np.array(slice_contours[1], dtype=np.float32)
+            if cv2.contourArea(slice_contours_0) > cv2.contourArea(slice_contours_1):
+                outer_contours.append(slice_contours[0])
+                inner_contours.append(slice_contours[1])
+            else:
+                inner_contours.append(slice_contours[0])            
+                outer_contours.append(slice_contours[1])
 
-    return contours
+    return inner_contours, outer_contours
 
 
-def save_wireframes_to_obj(contours, output_file, slice_spacing, affine):
+def save_wireframes(contours, output_file, affine, slice_spacing, format='obj'):
     """
     Save wireframes as points in an OBJ file, ensuring horizontal alignment
     and applying the affine transformation.
     """
     with open(output_file, 'w') as obj_file:
-        for i in range(0, len(contours), slice_spacing):
-            slice_contours = contours[i]
-            z = i  # Slice index used as the depth coordinate (Z in the horizontal plane)
-            for contour in slice_contours:
-                for point in contour:
-                    # Apply affine transformation
-                    x, y = point[1], point[0]
-                    coord = np.array([x, y, z, 1.0])
-                    transformed_coord = np.dot(affine, coord)[:3]
-                    obj_file.write(f"v {transformed_coord[0]} {transformed_coord[1]} {transformed_coord[2]}\n")
-
-
-def extract_points(data_scan, mask, affine, cnt_threshold=None):
-    # Find the indices of the voxels in the mask that are non-zero
-    mask_indices = np.array(np.nonzero(mask == 1)).T
-
-    # Convert these indices to physical coordinates
-    physical_points = nib.affines.apply_affine(affine, mask_indices)
-
-    # Optionally, retrieve the corresponding intensity values from the CT scan
-    intensities = data_scan[mask_indices[:, 0], mask_indices[:, 1], mask_indices[:, 2]]
-
-    # Limit to 5,000 points for visualization
-    if cnt_threshold is not None and physical_points.shape[0] > cnt_threshold:
-        idx_mask = np.random.choice(physical_points.shape[0], cnt_threshold, replace=False)
-        physical_points = physical_points[idx_mask]
-        intensities = intensities[idx_mask]
-    
-    return physical_points, intensities
-
-
-def create_point_cloud(points, intensities=None):
-    # Create an open3d point cloud object
-    pcd = o3d.geometry.PointCloud()
-
-    # Assign points
-    pcd.points = o3d.utility.Vector3dVector(points)
-    
-    # Optionally, assign intensities as colors (gray scale)
-    if intensities is not None:
-        colors = np.tile(intensities[:, np.newaxis], (1, 3))  # Repeat intensity values for R, G, B
-        colors = (colors - np.min(colors)) / (np.max(colors) - np.min(colors))  # Normalize to [0, 1]
-    
-    else:
-        default_color = [0.83, 0.83, 0.83]  # RGB for light gray
-        colors = np.tile(default_color, (len(points), 1))
-    
-    pcd.colors = o3d.utility.Vector3dVector(colors)
-    
-    return pcd
-
-
-def extract_mesh(mask, affine, threshold=0.5):
-    # Extract the surface mesh using marching cubes
-    verts, faces, _, _ = measure.marching_cubes(mask == 1, level=threshold)
-    
-    # Convert voxel indices to physical coordinates
-    physical_points = nib.affines.apply_affine(affine, verts)
-    
-    return physical_points, faces
-
-
-def create_mesh(vertices, faces):
-    # Create a trimesh object
-    mesh = trimesh.Trimesh(vertices=vertices, faces=faces)
-    return mesh
-
-
-def save_point_cloud(pcd, file_path):
-    o3d.io.write_point_cloud(file_path, pcd)
-
-
-def save_mesh(mesh, file_path):
-    mesh.export(file_path)
+        for slice_idx in range(0, len(contours), slice_spacing):
+            contour = contours[slice_idx]
+            z = slice_idx  # Slice index used as the depth coordinate (Z in the horizontal plane)
+            for point in contour:
+                # Apply affine transformation
+                x, y = point[1], point[0]
+                coord = np.array([x, y, z, 1.0])
+                transformed_coord = np.dot(affine, coord)[:3]
+                obj_file.write(f"v {transformed_coord[0]} {transformed_coord[1]} {transformed_coord[2]}\n")
 
 
 def generate_pcd(raw_data_dir_path, generated_data_dir_path):
-    image_paths = list((raw_data_dir_path / "images").glob("*.nii.gz"))
-    for image_path in tqdm(image_paths):
-        label_path = raw_data_dir_path / "labels" / image_path.name
-
-        # Load 3DE scan and mask files
-        MITEA_scan, MITEA_affine = load_nifti(str(image_path))
-        mask, mask_affine = load_nifti(str(label_path))
-
-        # Ensure both files have the same shape
-        assert MITEA_scan.shape == mask.shape
-        # Assert that the affine matrices are approximately equal
-        assert np.allclose(MITEA_affine, mask_affine)
-
-        # Extract point cloud from the mask
-        points, intensities = extract_points(MITEA_scan, mask, MITEA_affine, cnt_threshold=5000)
-
-        # Create the point cloud
-        MITEA_gt_pcd = create_point_cloud(points, intensities)
+    label_paths = list((raw_data_dir_path / "labels").glob("*.nii.gz"))
+    for label_path in tqdm(label_paths):
+        # Load mask files of 3DE scans
+        volume_data, affine = load_nifti(label_path)
+        inner_contours, outer_contours = extract_contours(volume_data)
 
         pcd_dir_path = generated_data_dir_path / "points"
         label_file_name = Path(label_path.stem).stem
+        inner_lv_wireframes_file_path = pcd_dir_path / (label_file_name + "_inner_" + ".obj")
+        outer_lv_wireframes_file_path = pcd_dir_path / (label_file_name + "_outer_" + ".obj")
 
-        # Save the point cloud to a file
-        save_point_cloud(MITEA_gt_pcd, str(pcd_dir_path / (label_file_name + ".ply")))
-
-        # Load the PLY file
-        pcd = trimesh.load(str(pcd_dir_path / (label_file_name + ".ply")), file_type='ply')
-
-        # Write to OBJ file
-        pcd.export(str(pcd_dir_path / (label_file_name + ".obj")), file_type='obj')
+        save_wireframes(inner_contours, inner_lv_wireframes_file_path, affine, slice_spacing=4)
+        save_wireframes(outer_contours, outer_lv_wireframes_file_path, affine, slice_spacing=4)
 
 
-def generate_pcd_and_mesh(raw_data_dir_path, generated_data_dir_path):
-    image_paths = list((raw_data_dir_path / "images").glob("*.nii.gz"))
-    for image_path in tqdm(image_paths):
-        label_path = raw_data_dir_path / "labels" / image_path.name
+def generate_mesh(raw_data_dir_path, generated_data_dir_path, smoothing_itr):
+    label_paths = list((raw_data_dir_path / "labels").glob("*.nii.gz"))
+    for label_path in tqdm(label_paths):
+        # Load mask files of 3DE scans
+        volume_data, affine = load_nifti(label_path)
+        
+        # Create a binary mask where the label is 1
+        lv_mask = (volume_data == 1)
 
-        # Load 3DE scan and mask files
-        MITEA_scan, MITEA_affine = load_nifti(str(image_path))
-        mask, mask_affine = load_nifti(str(label_path))
+        # Use the marching cubes algorithm to extract the surface mesh
+        verts, faces, normals, _ = measure.marching_cubes(lv_mask, level=0.5)
+        
+        # Apply affine transformation to the vertices
+        verts = np.dot(affine[:3, :3], verts.T).T + affine[:3, 3]
 
-        # Ensure both files have the same shape
-        assert MITEA_scan.shape == mask.shape
-        # Assert that the affine matrices are approximately equal
-        assert np.allclose(MITEA_affine, mask_affine)
+        # Create the mesh
+        mesh = trimesh.Trimesh(vertices=verts, faces=faces, vertex_normals=normals)
 
-        # Extract point cloud from the mask
-        points, intensities = extract_points(MITEA_scan, mask, MITEA_affine, cnt_threshold=5000)
-        MITEA_gt_pcd = create_point_cloud(points, intensities)
+        # Perform Laplacian smoothing
+        trimesh.smoothing.filter_laplacian(mesh, iterations=smoothing_itr)
 
-        # Extract mesh from the mask
-        vertices, faces = extract_mesh(mask, MITEA_affine)
-        MITEA_gt_mesh = create_mesh(vertices, faces)
-
-        # Define directories and filenames
-        pcd_dir_path = generated_data_dir_path / "points"
         mesh_dir_path = generated_data_dir_path / "meshes"
         label_file_name = Path(label_path.stem).stem
 
-        # Save the point cloud
-        save_point_cloud(MITEA_gt_pcd, str(pcd_dir_path / (label_file_name + ".ply")))
-
-        # Save the mesh
-        save_mesh(MITEA_gt_mesh, str(mesh_dir_path / (label_file_name + ".ply")))
-
-        # Optionally, save the mesh in .obj format
-        save_mesh(MITEA_gt_mesh, str(mesh_dir_path / (label_file_name + ".obj")))
+        # Save the mesh to a file
+        mesh.export(str(mesh_dir_path / (label_file_name + ".obj")))
 
 
 if __name__ == "__main__":
+    repo_dir_path = Path(__file__).resolve().parent.parent
     data_dir_path = repo_dir_path / "data" / "mitea"
     raw_data_dir_path = data_dir_path / "raw"
     generated_data_dir_path = data_dir_path / "generated"
-    # generate_pcd(raw_data_dir_path, generated_data_dir_path)
-    # print("Generating point cloud is done!")
 
-
-
-    # generate_pcd_and_mesh(raw_data_dir_path, generated_data_dir_path)
-    # print("Generating point clouds and meshes is done!")
-
-    file_path = com_dir = '../notebooks/MITEA_001_scan1_ED_label.nii.gz'
-    volume_data, affine = load_nifti(file_path)
-    lv_contours = extract_lv_contours(volume_data)
-    # generate_wireframes(lv_contours)
-    
-    save_wireframes_to_obj(lv_contours, 'lv_wireframes.obj', slice_spacing=4, affine=affine)
-
+    generate_pcd(raw_data_dir_path, generated_data_dir_path)
+    generate_mesh(raw_data_dir_path, generated_data_dir_path, smoothing_itr=50)
+    print("Generating point cloud is done!")
